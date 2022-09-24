b1d878c29be4b4afc52400f204482829390c5006
César Hilário <cesar.hilario.dev@gmail.com>
2022-09-23 21:05:09 -0300

    feat: project eggplant-brownie done

---
+++ ios-eggplant-brownie/eggplant-brownie/Models/Item.swift
@@ -0,0 +1,23 @@
+//
+//  Item.swift
+//  eggplant-brownie
+//
+//  Created by César Hilário on 23/09/22.
+//
+
+import UIKit
+
+class Item: NSObject {
+    
+    // MARK: - Atributos
+    
+    let nome: String
+    let calorias: Double
+    
+    // MARK: - Init
+    
+    init(nome: String, calorias: Double) {
+        self.nome = nome
+        self.calorias = calorias
+    }
+}
---
+++ ios-eggplant-brownie/eggplant-brownie/Models/Refeicao.swift
@@ -0,0 +1,37 @@
+//
+//  Refeicao.swift
+//  eggplant-brownie
+//
+//  Created by César Hilário on 23/09/22.
+//
+
+import UIKit
+
+class Refeicao: NSObject {
+    
+    // MARK: - Atributos
+    
+    let nome: String
+    let felicidade: Int
+    var itens: Array<Item> = []
+    
+    // MARK: - Init
+    
+    init(nome: String, felicidade: Int, itens: [Item] = []) {
+        self.nome = nome
+        self.felicidade = felicidade
+        self.itens = itens
+    }
+    
+    // MARK: - Metodos
+    
+    func totalDeCalorias() -> Double {
+        var total = 0.0
+        
+        for item in itens {
+            total += item.calorias
+        }
+        
+        return total
+    }
+}
